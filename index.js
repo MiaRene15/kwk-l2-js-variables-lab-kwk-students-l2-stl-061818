300acd46d5a7b0df89552b2098e3c8366eed312f
MiaRene15 <kids.hines@icloud.com>
2018-06-20 20:22:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 // Code your solution in this file!
 let companyName = 'Scuber';
 
-if companyName = 'specialCompany'{
-  console.log('TypeError')
-} else companyName = 'Scuber'{
-  console.log('Scuber')
-}
